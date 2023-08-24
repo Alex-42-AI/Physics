616ed9771eb2370dcf0f8dc00cd3b1eec17c8dbd
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2023-08-24 19:24:47 +0300

    Update CurvatureCalculator.py

--- Physics/CurvatureCalculator.py
+++ Physics/CurvatureCalculator.py
@@ -1,24 +1,24 @@
 from math import cos, acos, pi
-Earth_radius = 6371000
+EarthRadius = 6371000
 
 
 def horizon_drop_angle(viewer_elevation: float = 1.8):
-    return acos(Earth_radius / (Earth_radius + viewer_elevation))
+    return acos(EarthRadius / (EarthRadius + viewer_elevation))
 
 
 def horizon_distance(viewer_elevation: float = 1.8):
-    return horizon_drop_angle(viewer_elevation) * Earth_radius
+    return horizon_drop_angle(viewer_elevation) * EarthRadius
 
 
 def calculate_hidden(distance: float, viewer_elevation: float = 1.8):
     horizon = horizon_distance(viewer_elevation)
     if distance <= horizon:
         return 0
-    diff_angle = (distance - horizon) / Earth_radius
+    diff_angle = (distance - horizon) / EarthRadius
     if diff_angle >= pi / 2:
         return float('inf')
-    total_elevation_at_location = Earth_radius / cos(diff_angle)
-    return total_elevation_at_location - Earth_radius
+    total_elevation_at_location = EarthRadius / cos(diff_angle)
+    return total_elevation_at_location - EarthRadius
 
 
 print()
