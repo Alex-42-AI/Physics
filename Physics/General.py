b357000739135a67103fff44d4c8876ed1c6c173
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2024-11-10 03:56:46 +0200

    Update General.py

--- Physics/General.py
+++ Physics/General.py
@@ -3,9 +3,9 @@ from math import sqrt, sin, cos, radians
 g = 9.80665
 c = 299792458
 G = 6.673848e-11
-au = 149597870700
-SunMass = 1.9884849805923904e+30
 MoonDistance = 384300000
+au = 149597870700
+SunMass = 1.9884849805923904e30
 
 def time_to_fall(height: float, a: float = g):
     return sqrt(2 * height / a)
@@ -20,11 +20,10 @@ def maximal_reachable_height(v: float, a: float = g):
 
 
 def final_position_of_object_thrown_into_the_air(alpha: float, v: float, initial_elevation: float = 0, final_elevation: float = 0, a: float = g):
-    v_x, v_y = v * cos(radians(alpha)), v * sin(radians(alpha))
-    max_height = maximal_reachable_height(v_y, a)
+    max_height = maximal_reachable_height(v * sin(radians(alpha)), a)
     if final_elevation - initial_elevation > max_height:
         return
-    to_the_top = v_x * time_to_fall(max_height, a)
+    to_the_top = (v_x := v * cos(radians(alpha))) * time_to_fall(max_height, a)
     from_the_top = v_x * time_to_fall(max_height - final_elevation + initial_elevation, a)
     return to_the_top + from_the_top, final_elevation
 
